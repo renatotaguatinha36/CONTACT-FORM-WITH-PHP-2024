0b3d6d6837870e31c20a3ec33a3b3bd41e27f7b0
renatoguara2020 <renatoguara2020@gmail.com>
2024-04-06 20:46:27 -0300

    ok

--- connectionPDO.php
+++ connectionPDO.php
@@ -46,6 +46,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
   // prepare sql and bind parameters
   $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
