e5a53b32cb72c82d5985b14d9566a0be1060e6b0
renatoguara2020 <renatoguara2020@gmail.com>
2024-04-06 07:31:59 -0300

    OK

--- connectionPDO.php
+++ connectionPDO.php
@@ -10,6 +10,7 @@ try{
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+
   // sql to create table
   $sql = "CREATE TABLE MyGuests(
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -23,6 +24,38 @@ try{
   $conn->exec($sql);
   echo "Table MyGuests created successfully";
 }catch(PDOException $e){
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
+
+
+########################################## UPDATE #############################################################
+
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDBPDO";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
+  // Prepare statement
+  $stmt = $conn->prepare($sql);
+
+  // execute the query
+  $stmt->execute();
+
+  // echo a message to say the UPDATE succeeded
+  echo $stmt->rowCount() . " records UPDATED successfully";
+} catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
 
